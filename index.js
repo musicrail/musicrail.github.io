bddcd944681e845d748c00c1cea01cfb76ddfb9e
Torben <torben.pfohl@gmail.com>
2022-08-04 17:14:01 +0200

    Now with cats!

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 let quotesDiv = document.getElementById("quotes")
+let catDiv = document.getElementById("cat-pic")
+let catButton = document.getElementById("give-cat")
+
 fetch("https://api.kanye.rest")
 .then(res => res.json())
 .then(quote => {
     quotesDiv.innerHTML += `<p> ${quote.quote} </p>`
+})
+
+catButton.addEventListener("click", evt => {
+    fetch("https://api.thecatapi.com/v1/images/search?")
+    .then(res => res.json())
+    .then(cats => {
+        cats.forEach(cat => {
+            catDiv.innerHTML = `<h3>Some cat and a unicode cat &#128049; </h3>
+            <img src="${cat.url}" alt="kitty" />`
+        })
+    })
 })
