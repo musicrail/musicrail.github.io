25fb2dea3f690df5f5392fba770811b9e4288046
Torben <torben.pfohl@gmail.com>
2022-10-30 12:06:49 +0100

    change js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const title = document.querySelector("title")
 const button = document.querySelector("button")
 
-button.addEventListener("click", () => title.text("second stage"))
+button.addEventListener("click", () => title.text = "second stage")
